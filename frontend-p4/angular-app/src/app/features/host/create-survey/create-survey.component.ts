c26df4224713c9eef0b666d23838a9b2a49f36b7
Lincoln <lklim050@gmail.com>
2026-06-16 10:57:51 +0800

    preparing host functions
    create component for HOST functions
    create survey
    manage survey
    view creation

---
+++ frontend-p4/angular-app/src/app/features/host/create-survey/create-survey.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-survey',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-survey.component.html',
+  styleUrl: './create-survey.component.css'
+})
+export class CreateSurveyComponent {
+
+}
---
+++ frontend-p4/angular-app/src/app/features/host/manage-questions/manage-questions.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-questions',
+  standalone: true,
+  imports: [],
+  templateUrl: './manage-questions.component.html',
+  styleUrl: './manage-questions.component.css'
+})
+export class ManageQuestionsComponent {
+
+}
